ee5d06e1ebc9095eb68f7f4fd846276a35fcfe1a
JustDoom <justdoomdev@gmail.com>
2021-03-05 18:04:14 +1000

    code

---
+++ src/main/java/com/justdoom/dontrunwithscissors/main.java
@@ -0,0 +1,29 @@
+package com.justdoom.dontrunwithscissors;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("dontrunwith")
+public class main {
+    public static final String MOD_ID = "dontrunwith";
+    //private static final Logger LOGGER = LogManager.getLogger();
+
+    public main() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/justdoom/dontrunwithscissors/runEvent.java
@@ -0,0 +1,30 @@
+package com.justdoom.dontrunwithscissors;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ShearsItem;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class runEvent {
+
+    @SubscribeEvent
+    public static void event(LivingEvent.LivingUpdateEvent event) {
+        if (!(event.getEntity() instanceof PlayerEntity)) {
+            return;
+        }
+        if (event.getEntity().prevDistanceWalkedModified == event.getEntity().distanceWalkedModified) {
+            return;
+        }
+        PlayerEntity player = (PlayerEntity) event.getEntity();
+        if (!player.isSprinting()) {
+            return;
+        }
+        if (player.getHeldItemMainhand().getItem() instanceof ShearsItem && (int) (Math.random() * 10) == 0) {
+            player.attackEntityFrom(DamageSource.MAGIC, 2);
+        }
+    }
+}
